7f43fa1162cec3cd69fbb288829265432fba1baf
Gurmehar Singh Virdi <gurmeharsinghv@gmail.com>
2024-10-12 21:08:40 +0530

    patch functionality

--- REST_API/main.py
+++ REST_API/main.py
@@ -23,6 +23,11 @@ video_put_args.add_argument('name', type=str, help='Name of the video is require
 video_put_args.add_argument('views', type=int, help='Views of the video is required', required=True)
 video_put_args.add_argument('likes', type=int, help='Likes of the video is required', required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument('name', type=str, help='Name of the video is required')
+video_update_args.add_argument('views', type=int, help='Views of the video is required')
+video_update_args.add_argument('likes', type=int, help='Likes of the video is required')
+
 resource_fields = { # Serialize the instance
     'id': fields.Integer,
     'name': fields.String,
@@ -49,6 +54,23 @@ class Video(Resource):
         db.session.commit()
         return video, 201 
 
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message='Video does not exists, cannot update!')
+
+        if 'name' in args and args['name'] != None:
+            result.name = args['name']
+        if 'views' in args and args['views'] != None:
+            result.views = args['views']
+        if 'likes' in args and args['likes'] != None:
+            result.likes = args['likes']
+        
+        db.session.commit()
+
+        return result
     # def delete(self, video_id):
     #     abort_video_not_exist(video_id)
     #     del videos[video_id]
--- REST_API/test.py
+++ REST_API/test.py
@@ -2,14 +2,5 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-data = [{'name': 'Flask', 'views': 100,'likes': 150},
-        {'name': 'OpenAI', 'views': 10000,'likes': 20000},
-        {'name': 'Django', 'views': 1000,'likes': 10000}]
-
-for i in range(len(data)):
-    response = requests.put(BASE + 'video/' + str(i), data[i])
-    print(response.json())
-
-input()
-response = requests.get(BASE + 'video/4')
+response = requests.patch(BASE + 'video/2', {'views': 999})
 print(response.json())
